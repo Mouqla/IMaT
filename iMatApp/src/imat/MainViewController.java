4726f06f2cbb280146851bc77187aa0450de45b9
maxdreifeldt <maxdreifeldt02@gmail.com>
2023-05-09 16:30:33 +0200

    testbs

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
@@ -15,8 +16,6 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
 public class MainViewController implements Initializable {
 
     @FXML
-    Label pathLabel;
-    @FXML
     TextField searchBar;
     @FXML
     ImageView searchBarcloseImageView;
@@ -24,6 +23,8 @@ public class MainViewController implements Initializable {
     ImageView searchBarSearchImageView;
     @FXML
     ImageView closeButtonImageView;
+    @FXML
+    MenuButton menuButton1;
 
 
 
@@ -33,24 +34,24 @@ public class MainViewController implements Initializable {
 
         String iMatDirectory = iMatDataHandler.imatDirectory();
 
-        pathLabel.setText(iMatDirectory);
+
     }
 
 
     public void closeButtonMouseEntered(){
-        String imagePath = "/Users/mohammeduqla/Desktop/IMat/resources/iMatApp/src/imat/resources/icon_close_hover.png";
+        String imagePath = "./imat/resources/icon_close_hover.png";
         closeButtonImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(imagePath)));
     }
 
     @FXML
     public void closeButtonMousePressed(){
-        String imagePath = "/Users/mohammeduqla/Desktop/IMat/resources/iMatApp/src/imat/resources/icon_close_pressed.png";
+        String imagePath = "./imat/resources/icon_close_pressed.png";
         closeButtonImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(imagePath)));
     }
 
     @FXML
     public void closeButtonMouseExited(){
-        String imagePath = "/Users/mohammeduqla/Desktop/IMat/resources/iMatApp/src/imat/resources/icon_close.png";
+        String imagePath = "./imat/resources/icon_close.png";
         closeButtonImageView.setImage(new Image(getClass().getClassLoader().getResourceAsStream(imagePath)));
     }
 }
