138cde1260a2a2b02146502bae8739ba8103b6c8
maxdreifeldt <maxdreifeldt02@gmail.com>
2023-05-11 09:57:02 +0200

    Update ImatCategoryitem.java

--- iMatApp/src/imat/ImatCategoryitem.java
+++ iMatApp/src/imat/ImatCategoryitem.java
@@ -1,4 +1,11 @@
 package imat;
 
-public class ImatCategoryitem {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ImatCategoryitem extends AnchorPane {
+
+    @FXML
+    private Label categoryNameLabel;
 }
